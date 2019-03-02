549ad5236cfafd8102afb03d4f1aa458d21ae4ea
petr0n <petron@gmail.com>
2019-03-02 08:28:47 -0600

    init commit

---
+++ assets/js/giphy.js
@@ -0,0 +1,15 @@
+/* crystal collector */
+
+$(document).ready(function() {
+
+    var gifMadness = (function(){
+        
+        function init(){
+            return null;
+        };
+        init();
+    });
+    gifMadness();
+
+
+});
